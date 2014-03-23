16839bf9c7cb56ca899b9f14e3086d247f2a45b7
Kesavan Muthuvel <hi@kesavan.info>
2014-03-22 23:50:01 -0500

    Change for Version 1.2

--- MalarToon/MalarToon/src/main/java/info/kesavan/malartoon/MainActivity.java
+++ MalarToon/MalarToon/src/main/java/info/kesavan/malartoon/MainActivity.java
@@ -1,22 +1,32 @@
+/*
+    Update 2
+    2014-03-22
+ */
+
 package info.kesavan.malartoon;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    String answer;
 
     private WebView mWebView;
 
@@ -25,35 +35,40 @@ public class MainActivity extends ActionBarActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+
+        ConnectivityManager cm = (ConnectivityManager) getApplicationContext()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if (null != activeNetwork) {
+            if(activeNetwork.getType() == ConnectivityManager.TYPE_WIFI)
+                answer="You are connected to a WiFi Network";
+            if(activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE)
+                answer="You are connected to a Mobile Network\n" +
+                        "You may charged! ";
         }
-        String uri = "http://www.kesavan.info/";
-*/
+        else
+            answer = "No internet Connectivity.Please use Internet connection to use this app!";
+        Toast.makeText(getApplicationContext(), answer, Toast.LENGTH_LONG).show();
+
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
         // Enable Javascript
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-//        mWebView.loadUrl(uri);
 
         /**************************  TOON ACTION **********************/
         // Exit Code
         Button btn_toon = (Button) findViewById(R.id.button_open_toon);
         btn_toon.setOnClickListener(new View.OnClickListener() {
 
-        String uri = "http://www.kesavan.info/services/toon.php";
+            String uri = "http://www.kesavan.info/services/toon.php";
 
-         @Override
-         public void onClick(View v) {
-             mWebView.setWebViewClient(new WebViewClient());
-             mWebView.loadUrl(uri);
+            @Override
+            public void onClick(View v) {
+                mWebView.setWebViewClient(new WebViewClient());
+                mWebView.loadUrl(uri);
                 // TODO Auto-generated method stub
             }
         });
-
 
         /**************************  RAND ACTION **********************/
         // Exit Code
@@ -69,42 +84,11 @@ public class MainActivity extends ActionBarActivity {
                 // TODO Auto-generated method stub
             }
         });
-
-        /**************************  THANKS ACTION **********************/
-        // Exit Code
-        Button btn_thanks = (Button) findViewById(R.id.button_thanks);
-        btn_thanks.setOnClickListener(new View.OnClickListener() {
-
-            String uri = "http://www.kesavan.info/";
-
-            @Override
-            public void onClick(View v) {
-                mWebView.loadUrl(uri);
-                // TODO Auto-generated method stub
-            }
-        });
-
-
-
-        /**************************  EXIT ACTION **********************/
-        // Exit Code
-        Button btn_exit = (Button) findViewById(R.id.button_exit);
-        btn_exit.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                finish();
-                System.exit(0);
-            }
-        });
-
     }
 
-
-        @Override
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -117,8 +101,23 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            String uri = "http://www.kesavan.info/services/toon.php?choose=y";
+            mWebView.setWebViewClient(new WebViewClient());
+            mWebView.loadUrl(uri);
             return true;
         }
+
+        if(id == R.id.action_thanks){
+            String uri = "http://www.kesavan.info/feedback.php";
+                mWebView.loadUrl(uri);
+                // TODO Auto-generated method stub
+        }
+
+        if(id == R.id.action_exit){
+            finish();
+            System.exit(0);
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -132,10 +131,12 @@ public class MainActivity extends ActionBarActivity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+                                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
     }
 
 }
+
+
